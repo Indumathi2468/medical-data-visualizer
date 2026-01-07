c30dce37619e1e3d45a77fd9a0eb89031b3dd7ed
Indumathi2468 <indumathin339@gmail.com>
2026-01-07 13:10:15 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from medical_data_visualizer import draw_cat_plot, draw_heat_map
+
+# Run categorical plot
+cat_fig = draw_cat_plot()
+cat_fig.savefig("catplot.png")
+
+# Run heat map
+heat_fig = draw_heat_map()
+heat_fig.savefig("heatmap.png")
+
+print("Plots generated successfully")
